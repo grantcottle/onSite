0af221a9eef9f96edad0af9365e99b61c6acc247
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2018-10-19 17:00:39 -0700

    adding forms

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions'
+import { clearCurrentProfile } from '../../actions/profileActions'
+
 
 
 class Navbar extends Component {
 
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -17,6 +20,11 @@ class Navbar extends Component {
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
+       <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+                </Link>
+        </li>
         <li className="nav-item">
           <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
           <img 
@@ -80,7 +88,7 @@ class Navbar extends Component {
   }
 }
 
-Navbar.PropTypes = {
+Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 }
@@ -89,4 +97,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
