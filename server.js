e231665402eacd6e53f318e7e23fe7016b36177b
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2018-10-23 12:14:27 -0700

    prepared for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport')
-const PORT = process.env.PORT || 5000;
+const path = require('path')
+
 
 const properties = require('./routes/api/properties')
 const users = require('./routes/api/users')
@@ -37,6 +38,16 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/companies', companies)
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.user(express.static('/client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
